933f571760e9766f55bbd96f98adb073de2b9f86
Mate Soos <soos.mate@gmail.com>
2026-03-06 22:11:10 +0100

    cmake cleanup (#4)

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -1,4 +1,4 @@
 rm -rf CMake* src cmake* Make* sbva*
-emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL ..
+emcmake cmake -DSTATICCOMPILE=ON -DCMAKE_INSTALL_PREFIX=$EMINSTALL ..
 emmake make -j12
 emmake make install
