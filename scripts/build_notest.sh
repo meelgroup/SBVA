f1021c17dddee4fcd5d552969f459482907f9f84
Mate Soos <soos.mate@gmail.com>
2026-05-24 10:43:37 +0200

    Adding notest script

---
+++ scripts/build_notest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -rf CMake*
+rm -rf src
+rm -rf cmake*
+rm -rf Make*
+rm -rf sbva*
+rm -rf .cmake
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DENABLE_TESTING=OFF ..
+make -j$(nproc)
